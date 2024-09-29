95a9ece246c0598e8dd6514c9f251fd77673424b
saami0120 <saamisajid@gmail.com>
2024-09-29 03:21:40 -0400

    function updates

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+      let total = 0;
+        cart.forEach(item => {
+          total += parseFloat( item.cost.replace('$', '')) * item.quantity;
+        });
+        return total;
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping();
    
   };
 
 
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleIncrement = (item) => {
+            dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if ( item.quantity === 1) {
+            // dispatch the removeItem action reducer
+            dispatch(removeItem(item));
+      } else {
+          // dispatch the updateQuantity action reducer
+          dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      }
+
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.replace('$', '')) * item.quantity;
   };
 
   return (
